2fd8bde87ad893e823ed15765aad1988192b128b
er-anupamkumar <er.anupamkumar@gmail.com>
2026-01-04 19:51:43 +0100

    chore: create the app user controller

--- src/main/java/com/anupam/PediaTrackBackend/api/controller/AppUserController.java
+++ src/main/java/com/anupam/PediaTrackBackend/api/controller/AppUserController.java
@@ -1,31 +1,38 @@
 package com.anupam.PediaTrackBackend.api.controller;
 
+import com.anupam.PediaTrackBackend.api.service.AppUserRegistrationService;
 import com.anupam.pediatrackbackend.api.generated.model.AppUserCreateRequest;
 import com.anupam.pediatrackbackend.api.generated.model.AppUserResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/api/vi/users")
+@RequiredArgsConstructor
 public class AppUserController {
+
+  private final AppUserRegistrationService appUserRegistrationService;
+
 
   @PostMapping
   public ResponseEntity<AppUserResponse> registerNewAppUser(
       @RequestBody AppUserCreateRequest request) {
-    // Implementation for registering a new app user goes here
-    return ResponseEntity.ok(new AppUserResponse());
+    return appUserRegistrationService.registerNewAppUser(request);
   }
 
   @GetMapping("/{userId}")
-  public ResponseEntity<AppUserResponse> getAppUserProfile(@PathVariable Long userId) {
+  public ResponseEntity<AppUserResponse> getAppUserProfile(@PathVariable UUID userId) {
     // Implementation for retrieving an app user by ID goes here
-    return ResponseEntity.ok(new AppUserResponse());
+    return appUserRegistrationService.getAppUserProfile(userId);
   }
 
   @PutMapping("/{userId}")
   public ResponseEntity<AppUserResponse> updateAppUserProfile(
-      @PathVariable Long userId, @RequestBody AppUserCreateRequest request) {
+      @PathVariable UUID userId, @RequestBody AppUserCreateRequest request) {
     // Implementation for updating an app user goes here
-    return ResponseEntity.ok(new AppUserResponse());
+    return appUserRegistrationService.updateAppUserProfile(userId, request);
   }
 }
