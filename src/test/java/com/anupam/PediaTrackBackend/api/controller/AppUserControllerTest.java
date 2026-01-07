37455f1e48cea8ea529bfda5a1eaed5c68b598e9
er-anupamkumar <er.anupamkumar@gmail.com>
2026-01-07 14:26:40 +0100

    chore: add baseline for user controller tests

---
+++ src/test/java/com/anupam/PediaTrackBackend/api/controller/AppUserControllerTest.java
@@ -0,0 +1,31 @@
+package com.anupam.PediaTrackBackend.api.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class AppUserControllerTest {
+
+
+
+    @BeforeEach
+    void setUp() {
+      }
+
+    @AfterEach
+    void tearDown() {
+      }
+
+    @Test
+    void registerNewAppUser() {
+      }
+
+    @Test
+    void getAppUserProfile() {
+    }
+
+    @Test
+    void updateAppUserProfile() {
+      }
+}
